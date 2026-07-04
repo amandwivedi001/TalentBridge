61a3d446a32bb290787ebb2b90b0efe10dc4cf16
Aman Dwivedi <amandwivedi262@gmail.com>
2026-07-04 22:28:53 +0530

    added cookie option for backend production

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -90,12 +90,14 @@ export const login = asyncHandler(async (req, res) => {
 
   const token = generateToken(safeUser);
 
+  const isProduction = process.env.NODE_ENV === "production";
+
   const cookieOptions = {
-  httpOnly: true,
-  secure: false,
-  sameSite: "lax",
-  maxAge: 7 * 24 * 60 * 60 * 1000,
-};
+    httpOnly: true,
+    secure: isProduction,
+    sameSite: isProduction ? "none" : "lax",
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+  };
 
   return res
     .cookie(
